c52be8445213b9aa06922e7f1652977e5925859a
yexigufu <jyxc-dz-0100605@June.local>
2025-10-10 14:37:24 +0800

    commit scripts/autoregrssive

--- scripts/autoregressive/run_test.sh
+++ scripts/autoregressive/run_test.sh
@@ -4,7 +4,7 @@ export NODE_RANK=0
 export PROC_PER_NODE=8
 model_type='GPT-3B'
 rm -rf engine/__pycache__ tokenizer/tokenizer_image/__pycache__
-scripts/autoregressive/torchrun.sh test_net.py --vq-ckpt pretrained_models/vfmtok-tokenizer.pt            \
-    --gpt-ckpt snapshot/${model_type}/${model_type}-$1e.pt --compile --gpt-model ${model_type} --image-size 336 \
+scripts/autoregressive/torchrun.sh test_net.py --vq-ckpt DINOv2/tokenizer/vfmtok-tokenizer.pt            \
+    --gpt-ckpt DINOv2/${model_type}/${model_type}-$1e.pt --compile --gpt-model ${model_type} --image-size 336 \
     --sample-dir samples --image-size-eval 256 --cfg-scale $2 --precision bf16 --per-proc-batch-size $3   \
     --codebook-slots-embed-dim 12 --latent-size 16
